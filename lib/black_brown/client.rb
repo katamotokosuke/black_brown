1acbfe6ad235e233b7978692c8c8f5a617bef47e
katamoto kosuke local <katamotokosuke@gmail.com>
2017-06-03 17:19:26 +0900

    modify push method return value

--- lib/black_brown/client.rb
+++ lib/black_brown/client.rb
@@ -11,19 +11,18 @@ module BlackBrown
       yield(self) if block_given?
     end
 
-    #todo: implementation other than text
-    def push(user_id, message)
+    #user_id(require type: string): The id of the user you want to send to message
+    #messages(requre: array or string): Contents of the message to be sent
+    def push(user_id, messages)
       http_client.post do |request|
         request.url(PUSH_PATH)
-        request.headers = default_post_header
+        request.headers["Authorization"] = "Bearer #{channel_access_token}"
+        request.headers["User-Agent"] = "BlackBrown v#{BlackBrown::VERSION}"
         request.body = {
           to: user_id,
-          messages: [{
-            type: "text",
-            text: message
-          }]
+          messages: parse_messages(messages)
         }
-      end
+      end.success?
     end
 
     private
@@ -36,10 +35,16 @@ module BlackBrown
       end
     end
 
-    def default_post_header
-      {
-        "Authorization" => "Bearer #{channel_access_token}"
-      }
+    def parse_messages(messages)
+      res = []
+      if messages.is_a?(Array)
+        messages.each do |item|
+          res << { type: "text", text: item }
+        end
+      else
+        res << { type: "text", text: messages.to_s }
+      end
+      res
     end
   end
 end

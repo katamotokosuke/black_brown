a3d87235e8b213260428608d5169d91da3296abc
katamoto_kosuke <katamoto_kosuke@uniquevision.co.jp>
2017-06-05 08:49:29 +0900

    add rspec

--- spec/black_brown_spec.rb
+++ spec/black_brown_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe BlackBrown do
     expect(BlackBrown::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "'parse_messages' method works correctly when the argument is a character string " do
+    string_ary = BlackBrown::Client.new.send(:parse_messages, "hello world")
+    expect(string_ary).to eq [{type: "text", text: "hello world"}]
+  end
+
+  it "'parse_messages' method works correctly when the argument is a array " do
+    string_ary = BlackBrown::Client.new.send(:parse_messages, ["hello world", "shit"])
+    expect(string_ary).to eq [{type: "text", text: "hello world"}, {type: "text", text: "shit"}]
   end
 end
